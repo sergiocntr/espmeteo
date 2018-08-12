c0a7e0bcce9bd64578c7a0302de4c7c560340425
Sergio Contro <sergiocntr@gmail.com>
2018-08-12 23:11:28 +0200

    ok

--- ESPmeteo/src/cxonfig.example.h
+++ ESPmeteo/src/cxonfig.example.h
@@ -4,25 +4,25 @@
 static int default_sda_pin = 0;
 static int default_scl_pin = 2;
 //WIFI stuff
-const char* ssid     = "TIM-23836387";
-const char* password = "51vEBuMvmALxNQHVIHQKkn52";
-const char* webpass ="admin";
+const char* ssid     = "********";
+const char* password = "********";
+const char* webpass ="********";
 #include <ESP8266WiFi.h>
 IPAddress ip(192, 168, 1, 216); //Node static IP
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 // data web host Settings
-const char* host = "www.developteamgold.altervista.org";
+const char* host = "************";
 const int httpPort = 80;
 //voltage stuff
 //mqtt
-const char* nodeID="sergino";
+const char* nodeID="*******";
 const char* mqtt_server = "192.168.1.100";
 const uint16_t mqtt_port = 8883;
-const char* mqttUser ="sergio";
-const char* mqttPass ="donatella";
-const char* sensorsTopic = "/casa/esterno/sensori";
-const char* logTopic ="/casa/esterno/log"; // log generico per lo piu non usato
+const char* mqttUser ="********";
+const char* mqttPass ="********";
+const char* sensorsTopic = "**********";
+const char* logTopic ="***********"; // log generico per lo piu non usato
 
 //
 uint16_t voltage = 0;  //voltage get from attiny
